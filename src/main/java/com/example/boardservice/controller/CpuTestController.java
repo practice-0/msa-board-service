33ede7c0572c9ebed40a00ac4ab0bb48b6e8077a
jw <wjd10577@naver.com>
2026-02-08 12:12:41 +0900

    edit cpu test

--- src/main/java/com/example/boardservice/controller/CpuTestController.java
+++ src/main/java/com/example/boardservice/controller/CpuTestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CpuTestController {
     @GetMapping("/api/points/cpu-test")
     public String cpuTest() {
-        long end = System.currentTimeMillis() + 500;
+        long end = System.currentTimeMillis() + 5000;
 
         double sum = 0;
         while (System.currentTimeMillis() < end) {
